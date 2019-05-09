21eafd8a60a03642ba1a877309a7af8474a5b19a
Luke Hammer <textchimp@gmail.com>
2019-05-09 17:56:15 +1000

    wk1 day4: homework review, objects exercises, insult generator

---
+++ week1/shakespearean-insult-generator/js/main.js
@@ -0,0 +1,189 @@
+
+const firstAdjectives = [
+  'artless',
+  'bawdy',
+  'beslubbering',
+  'bootless',
+  'churlish',
+  'cockered',
+  'clouted',
+  'craven',
+  'currish',
+  'dankish',
+  'dissembling',
+  'droning',
+  'errant',
+  'fawning',
+  'fobbing',
+  'froward',
+  'frothy',
+  'gleeking',
+  'goatish',
+  'gorbellied',
+  'impertinent',
+  'infectious',
+  'jarring',
+  'loggerheaded',
+  'lumpish',
+  'mammering',
+  'mangled',
+  'mewling',
+  'paunchy',
+  'pribbling',
+  'puking',
+  'puny',
+  'qualling',
+  'rank',
+  'reeky',
+  'roguish',
+  'ruttish',
+  'saucy',
+  'spleeny',
+  'spongy',
+  'surly',
+  'tottering',
+  'unmuzzled',
+  'vain',
+  'venomed',
+  'villainous',
+  'warped',
+  'wayward',
+  'weedy',
+  'yeasty'
+];
+
+const secondAdjectives = [
+  'base-court',
+  'bat-fowling',
+  'beef-witted',
+  'beetle-headed',
+  'boil-brained',
+  'clapper-clawed',
+  'clay-brained',
+  'common-kissing',
+  'crook-pated',
+  'dismal-dreaming',
+  'dizzy-eyed',
+  'doghearted',
+  'dread-bolted',
+  'earth-vexing',
+  'elf-skinned',
+  'fat-kidneyed',
+  'fen-sucked',
+  'flap-mouthed',
+  'fly-bitten',
+  'folly-fallen',
+  'fool-born',
+  'full-gorged',
+  'guts-griping',
+  'half-faced',
+  'hasty-witted',
+  'hedge-born',
+  'hell-hated',
+  'idle-headed',
+  'ill-breeding',
+  'ill-nurtured',
+  'knotty-pated',
+  'milk-livered',
+  'motley-minded',
+  'onion-eyed',
+  'plume-plucked',
+  'pottle-deep',
+  'pox-marked',
+  'reeling-ripe',
+  'rough-hewn',
+  'rude-growing',
+  'rump-fed',
+  'shard-borne',
+  'sheep-biting',
+  'spur-galled',
+  'swag-bellied',
+  'tardy-gaited',
+  'tickle-brained',
+  'toad-spotted',
+  'unchin-snouted',
+  'weather-bitten'
+];
+
+const nouns = [
+  'apple-john',
+  'baggage',
+  'barnacle',
+  'bladder',
+  'boar-pig',
+  'bugbear',
+  'bum-bailey',
+  'canker-blossom',
+  'clack-dish',
+  'clotpole',
+  'coxcomb',
+  'codpiece',
+  'death-token',
+  'dewberry',
+  'flap-dragon',
+  'flax-wench',
+  'flirt-gill',
+  'foot-licker',
+  'fustilarian',
+  'giglet',
+  'gudgeon',
+  'haggard',
+  'harpy',
+  'hedge-pig',
+  'horn-beast',
+  'hugger-mugger',
+  'joithead',
+  'lewdster',
+  'lout',
+  'maggot-pie',
+  'malt-worm',
+  'mammet',
+  'measle',
+  'minnow',
+  'miscreant',
+  'moldwarp',
+  'mumble-news',
+  'nut-hook',
+  'pigeon-egg',
+  'pignut',
+  'puttock',
+  'pumpion',
+  'ratsbane',
+  'scut',
+  'skainsmate',
+  'strumpet',
+  'varlot',
+  'vassal',
+  'whey-face',
+  'wagtail'
+];
+
+
+// This function takes a maximum integer value, and returns
+// a random integer between 0 and max - 1 (it's -1 because of the .floor())
+const generateRandomInt = function( max ){
+  const random = Math.random() * max;
+  return Math.floor( random );
+};
+
+// This function takes an array as its argument, and returns
+// a randomly-chosen element from that array
+const getRandomElementFromArray = function( array ){
+  const randomIndex = generateRandomInt( array.length );
+  // console.log('*** in getRandomElementFromArray(): array length is: ', array.length);
+  // console.log('    ----- value to return: ', array[ randomIndex ]);
+  return array[ randomIndex ];
+  // return array[ generateRandomInt(array.length) ];
+};
+
+// TODO: how could this function take a numerical argument,
+// and print an insult that many times? i.e. generateInsult(5) prints out
+// 5 separate insults.
+const generateInsult = function(){
+  // debugger;  // start the debugger in the browser, when this line is reached
+  const firstAdjective = getRandomElementFromArray( firstAdjectives );
+  const secondAdjective = getRandomElementFromArray( secondAdjectives );
+  const noun = getRandomElementFromArray( nouns );
+
+  console.log(`Thou ${firstAdjective} ${secondAdjective} ${noun}!`);
+};
