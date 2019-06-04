94a1d3a4c2bb4f40b679f9c345a295e91a4d3840
Luke Hammer <textchimp@gmail.com>
2019-06-04 22:36:30 +1000

    Show page for movies

--- week5/rails-stocks-movies/app/controllers/movies_controller.rb
+++ week5/rails-stocks-movies/app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
 
   # These constants will be visible in every method
   # of this class
-  API_KEY = '______REDACTED______'
+  API_KEY = '___REDACTED___'
   BASE_URL = 'https://api.themoviedb.org/3'
 
   def form
@@ -10,17 +10,27 @@ class MoviesController < ApplicationController
 
   def search
 
-   url = "#{ BASE_URL }/search/movie?api_key=#{ API_KEY }&query=#{ params[:query] }"
+    url = "#{ BASE_URL }/search/movie?api_key=#{ API_KEY }&query=#{ params[:query] }"
 
-   # Print the API request URL we just constructed to the server
-   # console, so we can test it in the browser if we need to
-   puts url
+    # Print the API request URL we just constructed to the server
+    # console, so we can test it in the browser if we need to
+    puts url
 
-   response = HTTParty.get url
-   # raise 'hell'
+    response = HTTParty.get url
+    # raise 'hell'
 
-   @results = response['results']
+    @results = response['results']
 
+  end # search
+
+
+  def show
+    # raise 'hell'
+    url = "#{ BASE_URL }/movie/#{ params[:id] }?api_key=#{ API_KEY }"
+    puts url
+    @movie = HTTParty.get url
+    p @movie
   end
+
 
 end
